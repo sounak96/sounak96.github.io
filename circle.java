dfea738d0d26d49368344b3aee33bd873f30ce29
Sounak Banik <sounakbanik96@gmail.com>
2018-03-17 21:02:07 +0530

    Added circle.java and circle.class

---
+++ circle.java
@@ -0,0 +1,10 @@
+public class circle
+{
+	public static void main(String args[])
+	{
+		double radius=5;
+		final double pi=3.14159265;
+		double area= pi*radius*radius;
+		System.out.println("Area = "+area);
+	}
+}
